0e5590d50b9e366904279f3a9c4d62fdf7db3c7a
ayushisrv03-sketch <ayushisrv03@gmail.com>
2025-08-29 15:29:07 +0530

    Basics of Python

---
+++ Basic.py
@@ -0,0 +1,59 @@
+'''myVariableName= 'Ayushi'
+MyVariableName= 'Srivastava'
+my_variable_name= 'Rashi'
+print (myVariableName)'''
+
+#Many to multiple variable
+'''x,y,z=1,2,3
+print(id(x))
+print(id(y))
+print(id(z))'''
+
+#one to multiple
+'''x=y=z=2
+print(id(x))
+print(id(y))
+print(id(z))'''
+
+#unpacking
+'''f=["x","y","z"]
+a,b,c=f
+print(a)
+print(b)
+print(c)'''
+
+#global variable
+#there are two methods for global variable
+#1. Declare it globally 
+#2. If initializing a global variable in a specific function, use the keyword global to use it in the whole file
+'''
+HEAD
+x= "awesome"
+def myfunc():
+    print("Python is "+x)
+
+myfunc()
+
+
+x= "awesome"
+def myfunc():
+    print("Python is "+x)
+
+myfunc()'''
+
+#NUMERIC DATA TYPE
+'''var1= 1
+var2= True
+var3= 10.023
+var4= 10+3j
+print(type(var4))
+print(var4)'''
+
+#You can assign strings in single quotes as well as double quotes
+'''print('Hello')
+print("Hello")'''
+
+#To find the length of a string
+'''a="Hello World"
+print(len(a))
+dba54d4 (Added some datatypes)'''
