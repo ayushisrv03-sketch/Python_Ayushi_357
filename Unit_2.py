68fc87aace6a4bfa83f0e5d8f9e2fd2545a4f26a
ayushisrv03-sketch <ayushisrv03@gmail.com>
2025-09-03 09:38:12 +0530

    Operators in Python

---
+++ Unit_2.py
@@ -0,0 +1,41 @@
+#Taking input in python
+'''a=int(input("Enter your age:"))
+if(a>=18):
+    print("Eligible")
+else:
+    print("Not Eligible")'''
+
+#Using elseif conditions
+'''a=2
+if(a%2==0):
+    print("Even")
+else:
+    print("Odd")'''
+
+#shorthand if
+'''a=200
+b=33
+if(a>b): print("a is greater than b")'''
+
+#shorthand if_else
+'''a=2
+b=300
+print("A") if a>b else print("B")'''
+
+'''a=300
+b=300
+print("A") if a>b else print("=") if a==b else print("B")'''
+
+#Using and operator
+'''a=200
+b=30
+c=500
+
+if a>b and c>a:
+    print("both condition true")'''
+
+#using not operator
+'''a=33
+b=200
+if not a>b:
+    print("True")'''
