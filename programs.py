af973aac1119895af3cb36e6167b81a2cd9620ba
ayushisrv03-sketch <ayushisrv03@gmail.com>
2025-09-10 09:37:25 +0530

    Basic programs in python

---
+++ programs.py
@@ -0,0 +1,59 @@
+#Find square of a number
+'''print("Enter a number:")
+a=int(input())
+print(a*a)'''
+
+#WAP to take two numbers as input and print their sum
+'''print("Enter two numbers:")
+a=int(input())
+b=int(input())
+print(a+b)'''
+
+#WAP to swap two variables
+'''a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)'''
+
+#number guessing
+'''num=3
+print("Enter a no. in the range[1 to 10]:")
+a=int(input())
+if(num==a):
+    print("You guessed it right")
+else:
+    print("You guessed wrong. The number was "+str(num))'''
+
+#WAP to check if number is even or odd
+'''a=int(input("Enter a number:"))
+if(a%2==0):
+    print("Even")
+else:
+    print("Odd")'''
+
+#WAP to swap
+'''import math
+import numpy
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))'''
+
+#WAP to convert Celsius to Fahrenheit
+'''a=float(input("Enter temp in Celsius:"))
+b=(9/5*a)+32
+print(b)'''
+
+
+#WAP to check the type of a variable
+'''a=float(input("Enter first number:"))
+b=int(input("Enter second number:"))
+print(type(a),type(b))'''
+
+#WAP to calculate SI
+'''p=float(input("Enter principal:"))
+r=float(input("Enter rate of interest :"))
+t=float(input("Enter time:"))
+SI=(p*r*t)/100
+print(SI)'''
